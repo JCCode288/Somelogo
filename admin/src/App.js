1e4805151d18fffd0ac52844f95477af665f886e
JCCode288 <jendycaprico@gmail.com>
2023-03-13 16:20:40 +0700

    feat: add login page skeleton form feat: add listener to inputs issue: confused using json-server and fetchAPI

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,17 +3,48 @@ import Register from "./views/Register";
 import Dashboard from "./views/Dashboard";
 import Categories from "./views/Categories";
 import Products from "./views/Products";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [page, setPage] = useState("");
+
   return (
     <div className="App">
+      <nav className="flex">
+        <ul className="flex">
+          <li
+            className="cursor-pointer mx-2"
+            onClick={() => setPage("dashboard")}
+          >
+            dashboard
+          </li>
+          <li
+            className="cursor-pointer mx-2"
+            onClick={() => setPage("products")}
+          >
+            products
+          </li>
+          <li
+            className="cursor-pointer mx-2"
+            onClick={() => setPage("categories")}
+          >
+            categories
+          </li>
+          <li className="cursor-pointer mx-2" onClick={() => setPage("login")}>
+            login
+          </li>
+          <li className="cursor-pointer mx-2" onClick={localStorage.clear()}>
+            logout
+          </li>
+        </ul>
+      </nav>
       <div className="container">
         <h1 className="text-7xl">something here</h1>
-        <Login />
-        <Register />
-        <Dashboard />
-        <Categories />
-        <Products />
+        {page === "login" ? <Login page={page} setPage={setPage} /> : ""}
+        {page === "register" ? <Register /> : ""}
+        {page === "dashboard" ? <Dashboard /> : ""}
+        {page === "categories" ? <Categories /> : ""}
+        {page === "products" ? <Products /> : ""}
       </div>
     </div>
   );
--- admin/src/views/Login.js
+++ admin/src/views/Login.js
@@ -1,7 +1,59 @@
-function Login() {
+import { useEffect, useState } from "react";
+
+function Login({ setPage }) {
+  const [loginForm, setloginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function loginSubmit(e) {
+    e.preventDefault();
+    try {
+      let response = await fetch("http://localhost:3000/users");
+      let data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function emailHandler(e) {
+    const filledLogin = {
+      email: e.target.value,
+      password: loginForm.password,
+    };
+    setloginForm(filledLogin);
+  }
+
+  function passwordHandler(e) {
+    const filledLogin = {
+      email: loginForm.email,
+      password: e.target.value,
+    };
+    setloginForm(filledLogin);
+  }
+
   return (
     <div className="container">
-      <h1 className="text-6xl">this is login page</h1>
+      <form onSubmit={loginSubmit}>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={loginForm.email}
+          onChange={emailHandler}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          value={loginForm.password}
+          onChange={passwordHandler}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
