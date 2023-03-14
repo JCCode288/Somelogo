96e06c641c9415f7a6beb14a83e2b6ab5da7dcac
JCCode288 <jendycaprico@gmail.com>
2023-03-14 14:19:04 +0700

    git error

---
+++ admin/src/components/TableProduct.jsx
@@ -0,0 +1,92 @@
+import { toast } from "react-toastify";
+
+function TableProducts({ items }) {
+  const products = [...items];
+
+  function deleteProduct(id) {
+    const BASE_URL = "http://localhost:3001/products/";
+    fetch(`${BASE_URL}${id}`, {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("failed to delete");
+        }
+      })
+      .then((_) => toast.success("Products has been deleted"))
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  }
+
+  return (
+    <table className="table-auto hover:table-fixed focus:table-fixed">
+      <thead className="p-2">
+        <tr className="border-b-4 border-red-500">
+          <th className="text-center px-2">No.</th>
+          <th className="text-center px-2">Name</th>
+          <th className="text-center px-2">Description</th>
+          <th className="text-center px-2">Price</th>
+          <th className="text-center px-2">Image</th>
+          <th className="text-center px-2">Category</th>
+          <th className="text-center px-2">Author</th>
+          <th className="text-center px-2">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((el, index) => {
+          delete el.slug;
+          return (
+            <tr className="relative align-middle" key={el.id}>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50">
+                {++index}
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50 text-center">
+                {el.name}
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50 text-justify">
+                {el.description}
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50 text-center">
+                {el.price}
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50">
+                <div className="container flex justify-center">
+                  <img
+                    src={el.mainImg}
+                    alt="el.name"
+                    className="w-1/2 justify-center rounded-sm"
+                  />
+                </div>
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50 text-center">
+                {el.categoryId}
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50 text-center">
+                {el.authorId}
+              </td>
+              <td className="p-2 border-b-[1px] border-red-500 border-opacity-50 text-center">
+                <button
+                  className="flex h-fit w-fit p-2 border-[1px] rounded hover:bg-red-600 hover:text-white duration-150 ease-in mb-2"
+                  onClick={() => deleteProduct(el.id)}
+                >
+                  Delete
+                </button>
+                <button className="flex h-fit w-fit p-2 border-[1px] rounded hover:bg-red-600 hover:text-white duration-150 ease-in">
+                  Edit
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default TableProducts;
---
+++ admin/src/views/Login.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Login({ setPage }) {
+  const [loginForm, setloginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function loginSubmit(e) {
+    e.preventDefault();
+    try {
+      let response = await fetch("http://localhost:3001/users");
+
+      if (response.ok) {
+        let data = await response.json();
+        let user = data.find((el) => {
+          return (
+            el.email === loginForm.email && el.password === loginForm.password
+          );
+        });
+
+        if (user) {
+          toast("logged in!", {
+            pauseOnFocusLoss: false,
+            pauseOnHover: false,
+            autoClose: 500,
+          });
+          localStorage.logged = "true";
+          setPage("dashboard");
+        } else {
+          toast.error("wrong email/password", {
+            pauseOnFocusLoss: false,
+            pauseOnHover: false,
+            autoClose: 500,
+          });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function inputHandler(e) {
+    const filledLogin = { ...loginForm };
+    const { value, name } = e.target;
+    filledLogin[name] = value;
+
+    setloginForm(filledLogin);
+  }
+
+  return (
+    <div className="container">
+      <div className="container m-8 flex justify-center ">
+        <ToastContainer />
+        <form
+          onSubmit={loginSubmit}
+          className="p-8 border-2 shadow-sm border-black w-1/3"
+        >
+          <h1 className="text-3xl my-4">Login</h1>
+          <label className="">Email</label>
+          <div className="container my-2 flex w-full">
+            <input
+              className=" border-[1px] flex w-full p-2 border-red-500 border-opacity-50 hover:border-opacity-100 focus:border-opacity-100 focus:border-red-500"
+              type="text"
+              name="email"
+              value={loginForm.email}
+              onChange={inputHandler}
+            />
+          </div>
+          <label className="">Password</label>
+          <div className="container my-2 flex w-full ">
+            <input
+              className=" border-[1px] flex w-full p-2 border-red-500 border-opacity-50 hover:border-opacity-100 focus:border-opacity-100 focus:border-red-500"
+              type="password"
+              name="password"
+              value={loginForm.password}
+              onChange={inputHandler}
+            />
+          </div>
+          <button
+            type="submit"
+            className="my-4 py-2 px-4 rounded border-2 hover:bg-red-600 ease-linear duration-100 hover:text-white"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
