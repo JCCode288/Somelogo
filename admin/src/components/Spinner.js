e3a5d30279abf457fbef97f3ad9b544f91f6460a
JCCode288 <jendycaprico@gmail.com>
2023-03-14 10:29:51 +0700

    fix: spinner.js not added

--- admin/src/components/Spinner.js
+++ admin/src/components/Spinner.js
@@ -3,12 +3,12 @@ export default function Spinner() {
     <div class="flex items-center justify-center min-h-screen">
       <button
         type="button"
-        class="bg-indigo-400 h-max w-max rounded-lg text-white font-bold hover:bg-indigo-300 hover:cursor-not-allowed duration-[500ms,800ms]"
+        class="bg-red-500 h-max w-max rounded-lg text-white font-bold hover:bg-red-300 hover:cursor-not-allowed duration-[500ms,800ms]"
         disabled
       >
         <div class="flex items-center justify-center m-[10px]">
           <div class="h-5 w-5 border-t-transparent border-solid animate-spin rounded-full border-white border-4"></div>
-          <div class="ml-2"> Processing...</div>
+          <div class="ml-2"> Loading...</div>
         </div>
       </button>
     </div>
